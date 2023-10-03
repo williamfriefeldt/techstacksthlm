c4cba83fc3a91380a5ecb1408c4d5d355b315ba5
William Friefeldt <william.friefeldt@gmail.com>
2023-10-03 13:21:05 +0200

    Test without FB

--- src/server/database/init.ts
+++ src/server/database/init.ts
@@ -1,11 +1,8 @@
-import admin from 'firebase-admin';
-import { type App } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-
+/*
 const app: App = admin.initializeApp({
   credential: admin.credential.cert(
     JSON.parse(process.env['FIREBASE_SERVICE_ACCOUNT_KEY_JSON'] as string)
   ),
 });
 
-export const dataBase = getFirestore(app);
+export const dataBase = getFirestore(app); */
