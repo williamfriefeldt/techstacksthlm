e005c3c6d8fbcb4b76249a7387e098ebb40f3249
William <william.friefeldt@gmail.com>
2023-12-02 10:11:25 +0100

    No cache on fetch

--- src/app/pages/index.server.ts
+++ src/app/pages/index.server.ts
@@ -2,5 +2,5 @@ import { PageServerLoad } from '@analogjs/router';
 import { Company } from '../shared/entities';
 
 export const load = async ({ fetch }: PageServerLoad): Promise<Company[]> => {
-  return await fetch('/api/v1/companies');
+  return await fetch('/api/v1/companies', { cache: 'no-store' });
 };
